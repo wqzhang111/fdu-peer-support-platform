1a8de277b7be13ee2206c6f15db02097d0e98df8
evafengan <836183920@qq.com>
2024-06-18 19:23:18 -0700

    Add admin management
    admin management function

---
+++ springboot/src/main/java/com/example/controller/AdminController.java
@@ -0,0 +1,86 @@
+package com.example.controller;
+
+import com.example.common.Result;
+import com.example.entity.Admin;
+import com.example.service.AdminService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.web.bind.annotation.*;
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ *Administrator front-end operation interface
+ **/
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Resource
+    private AdminService adminService;
+
+    /**
+     * add
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody Admin admin) {
+        adminService.add(admin);
+        return Result.success();
+    }
+
+    /**
+     * delete
+     */
+    @DeleteMapping("/delete/{id}")
+    public Result deleteById(@PathVariable Integer id) {
+        adminService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     * bulk delete
+     */
+    @DeleteMapping("/delete/batch")
+    public Result deleteBatch(@RequestBody List<Integer> ids) {
+        adminService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    /**
+     * update
+     */
+    @PutMapping("/update")
+    public Result updateById(@RequestBody Admin admin) {
+        adminService.updateById(admin);
+        return Result.success();
+    }
+
+    /**
+     * search by ID
+     */
+    @GetMapping("/selectById/{id}")
+    public Result selectById(@PathVariable Integer id) {
+        Admin admin = adminService.selectById(id);
+        return Result.success(admin);
+    }
+
+    /**
+     * search all
+     */
+    @GetMapping("/selectAll")
+    public Result selectAll(Admin admin) {
+        List<Admin> list = adminService.selectAll(admin);
+        return Result.success(list);
+    }
+
+    /**
+     * sort by page
+     */
+    @GetMapping("/selectPage")
+    public Result selectPage(Admin admin,
+                             @RequestParam(defaultValue = "1") Integer pageNum,
+                             @RequestParam(defaultValue = "10") Integer pageSize) {
+        PageInfo<Admin> page = adminService.selectPage(admin, pageNum, pageSize);
+        return Result.success(page);
+    }
+
+}
---
+++ springboot/src/main/java/com/example/service/AdminService.java
@@ -0,0 +1,126 @@
+package com.example.service;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.example.common.Constants;
+import com.example.common.enums.ResultCodeEnum;
+import com.example.common.enums.RoleEnum;
+import com.example.entity.Account;
+import com.example.entity.Admin;
+import com.example.exception.CustomException;
+import com.example.mapper.AdminMapper;
+import com.example.utils.TokenUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * admin service
+ **/
+@Service
+public class AdminService {
+
+    @Resource
+    private AdminMapper adminMapper;
+
+    /**
+     * add
+     */
+    public void add(Admin admin) {
+        Admin dbAdmin = adminMapper.selectByUsername(admin.getUsername());
+        if (ObjectUtil.isNotNull(dbAdmin)) {
+            throw new CustomException(ResultCodeEnum.USER_EXIST_ERROR);
+        }
+        if (ObjectUtil.isEmpty(admin.getPassword())) {
+            admin.setPassword(Constants.USER_DEFAULT_PASSWORD);
+        }
+        if (ObjectUtil.isEmpty(admin.getName())) {
+            admin.setName(admin.getUsername());
+        }
+        admin.setRole(RoleEnum.ADMIN.name());
+        adminMapper.insert(admin);
+    }
+
+    /**
+     * delete
+     */
+    public void deleteById(Integer id) {
+        adminMapper.deleteById(id);
+    }
+
+    /**
+     * delete batch
+     */
+    public void deleteBatch(List<Integer> ids) {
+        for (Integer id : ids) {
+            adminMapper.deleteById(id);
+        }
+    }
+
+    /**
+     * update
+     */
+    public void updateById(Admin admin) {
+        adminMapper.updateById(admin);
+    }
+
+    /**
+     * search by id
+     */
+    public Admin selectById(Integer id) {
+        return adminMapper.selectById(id);
+    }
+
+    /**
+     * search all
+     */
+    public List<Admin> selectAll(Admin admin) {
+        return adminMapper.selectAll(admin);
+    }
+
+    /**
+     * search by page
+     */
+    public PageInfo<Admin> selectPage(Admin admin, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Admin> list = adminMapper.selectAll(admin);
+        return PageInfo.of(list);
+    }
+
+    /**
+     * log in
+     */
+    public Account login(Account account) {
+        Account dbAdmin = adminMapper.selectByUsername(account.getUsername());
+        if (ObjectUtil.isNull(dbAdmin)) {
+            throw new CustomException(ResultCodeEnum.USER_NOT_EXIST_ERROR);
+        }
+        if (!account.getPassword().equals(dbAdmin.getPassword())) {
+            throw new CustomException(ResultCodeEnum.USER_ACCOUNT_ERROR);
+        }
+        // 生成token
+        String tokenData = dbAdmin.getId() + "-" + RoleEnum.ADMIN.name();
+        String token = TokenUtils.createToken(tokenData, dbAdmin.getPassword());
+        dbAdmin.setToken(token);
+        return dbAdmin;
+    }
+
+    /**
+     * update password
+     */
+    public void updatePassword(Account account) {
+        Admin dbAdmin = adminMapper.selectByUsername(account.getUsername());
+        if (ObjectUtil.isNull(dbAdmin)) {
+            throw new CustomException(ResultCodeEnum.USER_NOT_EXIST_ERROR);
+        }
+        if (!account.getPassword().equals(dbAdmin.getPassword())) {
+            throw new CustomException(ResultCodeEnum.PARAM_PASSWORD_ERROR);
+        }
+        dbAdmin.setPassword(account.getNewPassword());
+        adminMapper.updateById(dbAdmin);
+    }
+
+}
